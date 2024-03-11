b9c9074e63d365f3e01ee82694ca8fc508de78ef
pd-ualr <99687158+pd-ualr@users.noreply.github.com>
2024-03-11 02:12:30 +0000

    improved select filter button & nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,23 @@ $(".filtering button").click(function () {
   }
   $(this).addClass("active");
 });
+
+
+// button select
+
+var button = document.getElementsByClassName("button");
+
+var addSelectClass = function () {
+  removeSelectClass();
+  this.classList.add("selected");
+};
+
+var removeSelectClass = function () {
+  for (var i = 0; i < button.length; i++) {
+    button[i].classList.remove("selected");
+  }
+};
+
+for (var i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", addSelectClass);
+}
